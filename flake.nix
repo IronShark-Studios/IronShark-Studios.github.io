d982f574a02c91e6ec87e8a7619dfdd654a5b6ba
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-03-08 02:23:38 +0000

    Built site for gh-pages

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Personal Blog Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+
+            # Scientific publishing engine
+            quarto
+            
+            # Required for executing Jupyter notebooks and Python code blocks!
+            python3
+            python3Packages.jupyter
+          ];
+
+          shellHook = ''
+            echo "=========================================="
+            echo "Quarto version: $(quarto --version)"
+            echo "Python version: $(python3 --version)"
+            echo "=========================================="
+            echo "Run 'quarto preview' to start the live server."
+          '';
+        };
+      }
+    );
+}
